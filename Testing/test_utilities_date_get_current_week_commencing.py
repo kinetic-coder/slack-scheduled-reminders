013cff03275fbc3c264eb9eae23f8ab9a079002c
kinetic-coder <ojbullock@googlemail.com>
2024-02-17 13:36:53 +0000

    Updated get_week_commencing_date unit tests to use a standard date format

--- Testing/test_utilities_date_get_current_week_commencing.py
+++ Testing/test_utilities_date_get_current_week_commencing.py
@@ -2,34 +2,36 @@ import pytest
 from datetime import datetime, timedelta
 from Utilities.Date import get_week_commencing_date  # replace with your actual module
 
+standard_date_format = "%d/%m/%Y"
+
 def test_get_week_commencing_date_with_a_monday_date_string():
     # Test for a date string that is already a Monday
     date = "26/02/2024"  # This is a Monday
     expected = datetime(2024, 2, 26)
-    assert get_week_commencing_date(date) == expected.strftime('%d/%m/%Y')
+    assert get_week_commencing_date(date) == expected.strftime(standard_date_format)
 
 def test_get_week_commencing_date_with_a_friday_date_string():
     # Test for a date string that is not a Monday
     date = "02/02/2024"  # This is a Friday
     expected = datetime(2024, 1, 29)  # The Monday of this week
-    assert get_week_commencing_date(date) == expected.strftime('%d/%m/%Y')
+    assert get_week_commencing_date(date) == expected.strftime(standard_date_format)
 
 def test_get_week_commencing_date_from_previous_month_with_a_wednesday_date_string():
     # Test for a date string that is not a Monday
     date = "01/05/2024"  # This is a Friday
     expected = datetime(2024, 4, 29)  # The Monday of this week
-    assert get_week_commencing_date(date) == expected.strftime('%d/%m/%Y')
+    assert get_week_commencing_date(date) == expected.strftime(standard_date_format)
 
 def test_get_week_commencing_date_with_monday_datetime():
     # Test for a datetime that is already a Monday
     date = datetime(2022, 1, 3)  # This is a Monday
-    assert get_week_commencing_date(date) == date.strftime('%d/%m/%Y')
+    assert get_week_commencing_date(date) == date.strftime(standard_date_format)
 
 def test_get_week_commencing_date_with_wednesday_datetime():
     # Test for a datetime that is not a Monday
     date = datetime(2022, 1, 5)  # This is a Wednesday
     expected = datetime(2022, 1, 3)  # The Monday of this week
-    assert get_week_commencing_date(date) == expected.strftime('%d/%m/%Y')
+    assert get_week_commencing_date(date) == expected.strftime(standard_date_format)
 
 def test_get_week_commencing_date_with_invalid_numeric():
 
